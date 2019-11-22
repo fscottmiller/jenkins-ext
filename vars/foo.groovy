6dda2404e1c425d4137200451f534c0c084c3581
Scott Miller <fsm5yd@virginia.edu>
2019-11-22 18:17:45 -0500

    mv

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,9 +1,9 @@
-def containers = [
-    kubectl: containerTemplate(name: 'kubectl', image: 'fscottmiller/kubectl', ttyEnabled: true, command: 'cat'),
-    helm: containerTemplate(name: 'helm', image: 'fscottmiller/helm', ttyEnabled: true, command: 'cat')
-]
-
 def call(String tool, Closure body) {
+    def containers = [
+        kubectl: containerTemplate(name: 'kubectl', image: 'fscottmiller/kubectl', ttyEnabled: true, command: 'cat'),
+        helm: containerTemplate(name: 'helm', image: 'fscottmiller/helm', ttyEnabled: true, command: 'cat')
+    ]
+
     if (!containers.keys().contains(tool)) {
         throw new Exception("${tool} is not a supported tool choice at this time. Please choose from ${containers.keys()}")
     }
