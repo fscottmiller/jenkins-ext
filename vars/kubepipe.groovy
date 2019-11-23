adec904b95f60853cb525241c3978af9f8be4283
Scott Miller <fsm5yd@virginia.edu>
2019-11-23 17:52:42 -0500

    try this

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -1,6 +1,17 @@
 import org.tools.*
 
 def call(Map options=[:], Closure body) {
+    def toolSet = Tools.getToolSet()
+    toolSet.keySet().each {
+        container -> toolSet[container]['commands'].keySet().each {
+            el -> def "${el}" = {
+                String cmd -> 
+                    // insert function code here
+                    println "${toolSet[container]['commands'][el]} ${cmd}"
+            }
+        }
+    }
+    
     Tools.getRequired().each {
         echo "Using ${it['name']} from ${it['image']}"
     }
@@ -8,20 +19,7 @@ def call(Map options=[:], Closure body) {
     echo "${options}"
     podTemplate(options) {
         node(POD_LABEL) {
-            script {
-                def toolSet = Tools.getToolSet()
-                toolSet.keySet().each {
-                    container -> toolSet[container]['commands'].keySet().each {
-                        el -> this.metaClass.methods["${el}"] = {
-                            String cmd -> 
-                                // insert function code here
-                                println "${toolSet[container]['commands'][el]} ${cmd}"
-                        }
-                    }
-                }
-                println "${this.metaClass.methods}"
-                body()
-            }
+            body()
         }
     }
 }
