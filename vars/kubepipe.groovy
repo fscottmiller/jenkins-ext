4987a92ff65a20e15d771f60f278348ea3fb4be1
Scott Miller <fsm5yd@virginia.edu>
2019-11-23 17:46:15 -0500

    binding was blocked...

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -8,30 +8,17 @@ def call(Map options=[:], Closure body) {
     echo "${options}"
     podTemplate(options) {
         node(POD_LABEL) {
-            println "----- ${this.getBinding().getClass()} -----"
-            // this.steps.functions.each {
-            //     println it.value.getFunctionName()
-            // }
             def toolSet = Tools.getToolSet()
-            def binding = new Binding()
-            this.getBinding().variables.each { k, v ->
-                binding.setVariable(k, v)
-            }
             toolSet.keySet().each {
                 container -> toolSet[container]['commands'].keySet().each {
-                    el -> this.getBinding().setVariable("${el}", {
+                    el -> this.metaClass.methods["${el}"] = {
                         String cmd -> 
                             // insert function code here
                             println "${toolSet[container]['commands'][el]} ${cmd}"
-                    })
+                    }
                 }
             }
-            this.setBinding(binding)
-            println "===== Binding Variables ======"
-            this.getBinding().variables.each {
-                println it
-                println it.value
-            }
+
             body()
         }
     }
