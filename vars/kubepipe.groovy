dec050ec96aadf684a300b527add635a5a42f8e9
Scott Miller <fsm5yd@virginia.edu>
2019-11-22 19:50:20 -0500

    test

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -1,18 +1,14 @@
 import org.tools.*
 import org.jenkinsci.plugins.pipeline.modeldefinition.model.Stages
 
-def stages(Closure body) {
-    body()
-}
-
-def steps(Closure body) {
-    body()
-}
-
 def call(Closure body) {
-    podTemplate(containers: Tools.get()) {
-        node(POD_LABEL) {
-            body()
+    pipeline { 
+        agent {
+            kubernetes {
+                podTemplate(containers: Tools.get()) {
+                }
+            }
         }
+        body()
     }
 }
