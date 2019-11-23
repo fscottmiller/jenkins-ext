312662b9652eb3d35d419989a223ff512fa2c841
Scott Miller <fsm5yd@virginia.edu>
2019-11-22 20:22:07 -0500

    again

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -2,7 +2,6 @@ import org.tools.*
 
 def call(Closure body) {
     podTemplate(containers: Tools.getRequired()) {
-        echo Tools.getRequired()
         node(POD_LABEL) {
             body()
         }
