adf456518eb9a262332a63832c9924797a150ed4
Scott Miller <fsm5yd@virginia.edu>
2019-11-23 13:49:35 -0500

    syntax

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -1,7 +1,7 @@
 import org.tools.*
 
 def call(Closure body) {
-    Tools.setToolSet(readYaml text: libraryResource('org/tools/toolSet.yaml'))
+    Tools.setToolSet(readYaml(text: libraryResource('org/tools/toolSet.yaml')))
     podTemplate(containers: Tools.getRequired()) {
         node(POD_LABEL) {
             body()
