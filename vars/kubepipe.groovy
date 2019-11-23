adf0b3ac9aa6c9fba90fd6c30d98ed9570bd85ae
Scott Miller <fsm5yd@virginia.edu>
2019-11-23 17:20:42 -0500

    debug

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -8,7 +8,7 @@ def call(Map options=[:], Closure body) {
     echo "${options}"
     podTemplate(options) {
         node(POD_LABEL) {
-            println "----- ${this.getBinding().variables.getClass()} -----"
+            println "----- ${this.methods} -----"
             // this.steps.functions.each {
             //     println it.value.getFunctionName()
             // }
