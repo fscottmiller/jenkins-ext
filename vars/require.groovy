8ffdeb5ecaa1d639e0bf501a6f61cef4cb53ecf6
Scott Miller <fsm5yd@virginia.edu>
2019-11-22 20:35:23 -0500

    try

--- vars/require.groovy
+++ vars/require.groovy
@@ -8,10 +8,11 @@ def call(String tool) {
 
 def call(Map tool) {
     tool.keySet.each {
+        it = it.toLowerCase()
         if (!['name', 'version'].contains(it)) {
             throw new Exception("Please only specify name and version")
         }
     }
-    Tools.require tool
+    Tools.require tool['name']
 }
 
