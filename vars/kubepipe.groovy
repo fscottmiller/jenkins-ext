fed508f27b7a36699e902b8d9d22502349e68f76
Scott Miller <fsm5yd@virginia.edu>
2019-11-23 14:00:57 -0500

    add debug print

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -1,6 +1,7 @@
 import org.tools.*
 
 def call(Closure body) {
+    echo "Using toolset ${Tools.getRequired()}"
     podTemplate(containers: Tools.getRequired()) {
         node(POD_LABEL) {
             body()
