3c9c586b58645c51e861d42a461fb1d228e818b9
Scott Miller <fsm5yd@virginia.edu>
2019-11-25 16:09:47 -0500

    try .value

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -1,7 +1,7 @@
 def call(parent) {
     def tools = readYaml(text: libraryResource('org/tools/toolSet.yaml'))
     tools.each {
-        tool -> tool['commands'].each {
+        tool -> tool.value['commands'].each {
             command -> parent."${command.key}" = { String input -> 
                 container(tool) {
                     sh script: "${command} ${input}", returnStdout: true
