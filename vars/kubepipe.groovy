29703cbc8e7365b9eea82b5062711b6558d09a0d
Scott Miller <fsm5yd@virginia.edu>
2019-11-25 14:47:47 -0500

    syntax

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -1,8 +1,8 @@
 import org.tools.*
 
 def call(Map options=[:], Closure body) {
-    println getDelegate()
-    println getOwner()
+    println this.getDelegate()
+    println this.getOwner()
     options['containers'] = Tools.getRequired()
     podTemplate(options) {
         node(POD_LABEL) {
