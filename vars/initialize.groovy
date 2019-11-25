3b225aa0bbf126dcede8cb755a32d52c563dcb19
Scott Miller <fsm5yd@virginia.edu>
2019-11-25 16:12:54 -0500

    find props

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -2,7 +2,7 @@ def call(parent) {
     def tools = readYaml(text: libraryResource('org/tools/toolSet.yaml'))
     tools.each {
         tool -> tool.value['commands'].each {
-            command -> echo "${command.getClass()}"; parent."${command.key}" = { String input -> 
+            command -> echo "${command.getProperties()}"; parent."${command.key}" = { String input -> 
                 container(tool) {
                     sh script: "${command} ${input}", returnStdout: true
                 }
