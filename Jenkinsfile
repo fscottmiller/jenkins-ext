9a30e2f11e5216c93b8f95b497688881c6435e8f
Scott Miller <fsm5yd@virginia.edu>
2019-11-25 16:17:32 -0500

    test - does this work

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,15 @@
 @Library('jenkins-ext@dynamicFunctions') _
 
 require 'python'
+require 'nodejs'
+require 'ruby'
 initialize this
 
 kubepipe(serviceAccount: 'jenkins-admin') {
     def version = python "--version"
     echo version
+
+    npm "--version"
+    nodejs "--version"
+    ruby "--version"
 }
--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -2,10 +2,7 @@ def call(parent) {
     def tools = readYaml(text: libraryResource('org/tools/toolSet.yaml'))
     tools.each {
         tool -> tool.value['commands'].each {
-            command -> 
-                echo "${tool.getClass()}"
-                echo "${tool.key.getClass()}"
-                parent."${command.key}" = { String input -> 
+            command -> parent."${command.key}" = { String input -> 
                 container(tool.key) {
                     sh script: "${command.value} ${input}", returnStdout: true
                 }
