e544787f331828157cb03698e6082672edc450e1
Scott Miller <fsm5yd@virginia.edu>
2019-11-25 14:48:51 -0500

    test

--- vars/kubepipe.groovy
+++ vars/kubepipe.groovy
@@ -1,11 +1,15 @@
 import org.tools.*
 
 def call(Map options=[:], Closure body) {
-    println this.getDelegate()
-    println this.getOwner()
+    // println getDelegate()
+    // println getOwner()
     options['containers'] = Tools.getRequired()
     podTemplate(options) {
         node(POD_LABEL) {
+            def commands = ['ruby', 'python', 'nodejs']
+            commands.each {
+                tool -> this."${tool}" = { println tool }
+            }
             body()
         }
     }
