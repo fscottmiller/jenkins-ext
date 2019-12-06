fbab86d9452dda8a9654c5dc48f5c5c849d22645
Scott Miller <fsm5yd@virginia.edu>
2019-12-05 20:22:47 -0500

    debug

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -1,9 +1,6 @@
 def call(parent) {
     def tools = readYaml(text: libraryResource('org/tools/toolSet.yaml'))
     tools.each {
-        echo "${it} -- ${it.value}"
-    }
-    tools.each {
         tool -> tool.value['commands'].each {
             command -> parent."${command.key}" = { String input -> 
                 container(tool.key) {
