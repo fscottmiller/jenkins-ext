e021b30e441c24d6eb01403b3c7b62f3ddcc5562
Scott Miller <scottmillerjr@gmail.com>
2019-12-16 14:05:15 -0500

    try again

--- vars/kube.groovy
+++ vars/kube.groovy
@@ -2,10 +2,10 @@ import org.tools.*
 import org.yaml.snakeyaml.Yaml
 
 def call(Map options=[:]) {
-    options['spec'] = [:]
-    options['spec']['containers'] = Tools.getRequired()
     options['apiVersion'] = 'v1'
     options['kind'] = 'Pod'
+    options['spec'] = [:]
+    options['spec']['containers'] = Tools.getRequired()
     // echo "${options.getClass()}"
     // echo "${options}"
     def yaml = new Yaml().dump(options)
