25bed053447c2c720977a876a7dec7dc45a08fa2
Scott Miller <scottmillerjr@gmail.com>
2019-12-16 14:03:40 -0500

    conform yaml template

--- vars/kube.groovy
+++ vars/kube.groovy
@@ -2,7 +2,10 @@ import org.tools.*
 import org.yaml.snakeyaml.Yaml
 
 def call(Map options=[:]) {
-    options['containers'] = Tools.getRequired()
+    options['spec'] = [:]
+    options['spec']['containers'] = Tools.getRequired()
+    options['apiVersion'] = 'v1'
+    options['kind'] = 'Pod'
     // echo "${options.getClass()}"
     // echo "${options}"
     def yaml = new Yaml().dump(options)
